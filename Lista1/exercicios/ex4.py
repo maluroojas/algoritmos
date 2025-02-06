8b0504d160729d019c357b1d8938f780522d56a8
maluroojas <127432742+maluroojas@users.noreply.github.com>
2025-02-06 18:34:07 -0400

    Delete Lista1/exercicios directory

--- Lista1/exercicios/ex4.py
+++
@@ -1,9 +0,0 @@
-def nomes_maiores_que_cinco(nomes):
-    # Usamos 'filter' com lambda para pegar nomes com mais de 5 letras.
-    return list(filter(lambda nome: len(nome) > 5, nomes))
-
-# Testando a função com uma lista de exemplo
-nomes = ["Ana", "Lucas", "Fernanda", "João"]
-resultado = nomes_maiores_que_cinco(nomes)
-
-print(resultado)  
